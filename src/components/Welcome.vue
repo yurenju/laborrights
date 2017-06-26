b3f368e1ee400e5e9aaad7df826eabb861941234
Yuren Ju (yurenju) <yurenju@gmail.com>
2017-06-26 10:11:46 +0800

    add notice for alpha

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,6 +4,11 @@
     <p>按下 <strong>開始</strong> 來了解你的權利！
     <p>免責聲明：本軟體所提供的資訊僅為參考用，若有任何法律上的疑問請洽專業單位。</p>
 
+    <div class="alert alert-info" role="alert">
+      本網站仍在開發階段，資訊計算難免有誤，有遇到任何問題，歡迎使用選單中的 <a href="https://goo.gl/forms/4dRTCJbWnkaiWZ3v2" target="_blank">意見回饋</a> 告訴我們！
+    </div>
+
+
     <p class="text-center">
       <router-link class="btn btn-primary" to="/profile">開始</router-link>
     </p>
